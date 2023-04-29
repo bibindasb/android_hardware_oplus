3a47f5fc08f5c417555aab20c5f4828b73ce23f4
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-29 04:51:55 +0000

    power-libperfmgr: create a split product makefile
    Change-Id: I45b16621a9431552507bf5efb17572d31be8de2c
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ aidl/power-libperfmgr/power_product.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 StatiXOS
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Product makefile for power-libperfmgr for oplus devices
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
+
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.oplus-libperfmgr
+
